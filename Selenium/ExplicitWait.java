525d3f8bcd41b83ff08407f775c661d6b0867a1c
Rustem <48765033+RustemCoder@users.noreply.github.com>
2022-06-09 18:17:39 -0500

    Update and rename SeleniumExplicitWait.java to ExplicitWait.java

--- Selenium/ExplicitWait.java
+++ Selenium/ExplicitWait.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-public class SeleniumExplicitWait {
+public class ExplicitWait {
 
   // created reference variable for WebDriver
   WebDriver driver;
