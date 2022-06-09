9651ab907d78e44bae4f364cfd392103049f498b
Rustem <48765033+RustemCoder@users.noreply.github.com>
2022-06-09 18:17:58 -0500

    Update and rename implicitWait.java to ImplicitWait.java

--- Selenium/ImplicitWait.java
+++ Selenium/ImplicitWait.java
@@ -5,7 +5,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-class SeleniumImplicitWait{
+class ImplicitWait{
   private WebDriver driver;
   private String baseUrl;
   private WebElement element;
